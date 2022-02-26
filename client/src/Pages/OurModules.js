336d0de6e52811f6c5ae489d7b54630bc5f13748
Justin <jjasa.2000@gmail.com>
2022-02-26 16:18:08 +1100

    styling the card

--- client/src/Pages/OurModules.js
+++ client/src/Pages/OurModules.js
@@ -1,13 +1,52 @@
 import React from 'react';
 import ModuleCard from '../Components/ModuleCard';
 
-function OurModules() {
+const modules = [
+	{
+		module: '1',
+		module_name: 'Water Resources and Accessibility',
+	},
+	{
+		module: '2',
+		module_name: 'Water Health and Safety',
+	},
+	{
+		module: '3',
+		module_name: 'Water Sustainability',
+	},
+	{
+		module: '4',
+		module_name: 'Sanitation',
+	},
+	{
+		module: '5',
+		module_name: 'Hand Hygiene',
+	},
+	{
+		module: '6',
+		module_name: 'Disinfection and Other Hygienic Practices',
+	},
+];
+
+function OurModules({ modules }) {
+	console.log(modules);
+
 	return (
 		<div>
-			<div className="flex flex-col justify-center items-center text-h1-big font-Bebas">
+			<div className="flex flex-col justify-center items-center text-h1-big text-Bebas">
 				Our Modules
 			</div>
-			<ModuleCard />
+			<div className="flex flex-col justify-evenly">
+				<ModuleCard module={1} name={'Water Resources and Accessibility'} />
+				<ModuleCard module={2} name={'Water Health and Safety'} />
+				<ModuleCard module={3} name={'Water Sustainability'} />
+				<ModuleCard module={4} name={'Sanitation'} />
+				<ModuleCard module={5} name={'Hand Hygiene'} />
+				<ModuleCard
+					module={6}
+					name={'Disinfection and Other Hygienic Practices'}
+				/>
+			</div>
 		</div>
 	);
 }
--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,15 +1,17 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        Roboto: ['Roboto'],
-        BebasNeue: ['Bebas'],
-      },
-    },
-  },
-  plugins: [],
-}
-  
+	content: ['./src/**/*.{js,jsx,ts,tsx}'],
+	theme: {
+		extend: {
+			fontFamily: {
+				Roboto: ['Roboto'],
+				BebasNeue: ['Bebas'],
+			},
+			fontSize: {
+				'h1-large': '72px',
+				'text-small': '18px',
+				'text-medium': '24px',
+			},
+		},
+	},
+	plugins: [],
+};
